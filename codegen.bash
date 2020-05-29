7d603a0db95f7084357bfcd56e49ebcd52bb1b05
yezhuoyang <yezhuoyang@sjtu.edu.cn>
2020-05-29 09:59:43 +0800

    .

--- codegen.bash
+++ codegen.bash
@@ -1,7 +1,7 @@
 set -e
 cd "$(dirname "$0")"
 export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./lib/commons-text-1.6.jar:./bin:./lib/commons-lang3-3.8.1.jar   compilerjava.Main "
-cat > code.txt
+#cat > code.txt
 $CCHK
 
 
